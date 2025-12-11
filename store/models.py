2bd12dfab05e6fc497c8d5f2436bcecd3cf219d0
HimaVamsi G <himavamsigumpu@gmail.com>
2025-12-12 00:16:55 +0530

    user login.html dashboard.html logout view by using django built in authentication and autherization

--- store/models.py
+++ store/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class UserAccount(models.Model):
+    name = models.CharField(max_length=150)
+    username = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    mobile = models.CharField(max_length=15)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
--- store/views.py
+++ store/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
-# Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            messages.error(request, "Invalid credentials")
+            return redirect("login")
+
+        login(request, user)
+        return redirect("dashboard")
+
+    return render(request, "login.html")
+
+
+
+
+@login_required(login_url="login")
+def dashboard(request):
+    return render(request, "dashboard.html")
+
+
+
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
